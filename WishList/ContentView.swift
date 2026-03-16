6d9bec1b0cd11b2c1705fcdb3aad5607bd91babf
Jose <jeduramos7001@gmail.com>
2026-03-15 18:50:55 -0700

    Initial commit

---
+++ WishList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WishList
+//
+//  Created by Jose Ramos on 15/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Jose Ramos on 15/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
